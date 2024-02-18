1c057e52011122c7db799c29998eb4e8394a45fc
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-18 20:25:02 +0600

    Making the methods more dynamic

--- oop/SavingsAccount.java
+++ oop/SavingsAccount.java
@@ -7,40 +7,39 @@ public class SavingsAccount {
 
     //blance checking method :
     public void checkBalance(){
-        System.out.println("Hello!");
-        System.out.println("Your balance is: " + balance);
+        System.out.println("your balance is: " + balance);
     }
 
     //withdraw method : 
-    public void withdraw(){
-        int afterWithdraw = balance - 300;
+    public void withdraw(int withdrawAmount){
+        int afterWithdraw = balance - withdrawAmount;
         balance = afterWithdraw;
-        System.out.println("You just withdraw: " + 300); 
+        System.out.println("You just withdraw: " + withdrawAmount + ". So now, "); 
     }
 
     // Deposit method : 
-    public void deposit(){
-        int afterDeposit = balance + 600;
+    public void deposit(int depositAmount){
+        int afterDeposit = balance + depositAmount;
         balance = afterDeposit;
-        System.out.println("You just deposit: " + 600); 
+        System.out.println("You just deposit: " + depositAmount + ". So now, "); 
     }
 
     public static void main(String[] args){
         SavingsAccount savings = new SavingsAccount(2000);
         
         //intial balacne check
-        System.out.println("Your current initial balance is: " + savings.balance);
+        System.out.println("Your initial balance is: " + savings.balance);
 
         System.out.println("------------------------------------------");
 
         // After deposit : 
-        savings.deposit();
+        savings.deposit(700);
         savings.checkBalance();
 
         System.out.println("------------------------------------------");
 
         // After withdraw :
-        savings.withdraw();
+        savings.withdraw(500);
         savings.checkBalance();
 
     }
