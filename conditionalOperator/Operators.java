cb85b25453ec78b6af459ae9fb312d3e1da84502
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-29 08:37:07 +0600

    Make comments to avoid errors

--- conditionalOperator/Operators.java
+++ conditionalOperator/Operators.java
@@ -5,8 +5,8 @@ public class Operators {
         int a = 6;
         int b = 3;
 
-        boolean ex1 = !(a == 7 && (b >= a || a != a));
-        System.out.println(ex1); //true
+        // boolean ex1 = !(a == 7 && (b >= a || a != a));
+        // System.out.println(ex1); //true
 
         boolean ex2 = a == b || !(b > 3);
         System.out.println(ex2); //true
