591902668f232d0d3478d7faa9ee7aa000a7082d
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-29 08:35:07 +0600

    Mixing conditional operators

---
+++ conditionalOperator/Operators.java
@@ -0,0 +1,17 @@
+package conditionalOperator;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+
+        boolean ex1 = !(a == 7 && (b >= a || a != a));
+        System.out.println(ex1); //true
+
+        boolean ex2 = a == b || !(b > 3);
+        System.out.println(ex2); //true
+    
+        boolean ex3 = !(b <= a && b != a + b);
+        System.out.println(ex3); //false
+    }
+}
